52d586951af973901b20a037faf2b68ffddc4315
Richard Murray <rfm@yorku.ca>
2024-01-08 19:04:17 -0500

    posted lecture code

---
+++ 01b - lists/lists.py
@@ -0,0 +1,146 @@
+# lists.py  Lists and tuples
+
+# 1. Variables and basic mathematical operations
+
+x = 3.0
+y = 4.0
+
+z = x + y
+z = x - y
+z = x * y
+z = x / y
+z = x ** y
+z = ( x + y ) * y
+
+# mathematical functions
+
+import math
+
+z = math.sin(x)
+z = math.log(x)
+z = math.exp(x)
+z = math.pi
+
+# 2. Data types
+
+# float
+x = 3.1415
+type(x)
+y = 0.0
+type(y)
+
+# int
+y = 0
+type(y)
+
+# bool
+x = True
+type(x)
+y = False
+type(y)
+
+# 3. Lists
+
+x = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]  # a list
+type(x)
+
+# many of our examples will be lists of numbers, but lists can contain
+# any data type, including other lists
+y = [0, 0.0, True, [10,20,30]]
+type(y)
+
+# - index
+
+x[0]   # elements are numbered starting at zero
+x[1]
+x[4]
+x[20]  # error
+
+# - slice
+
+x[0:3]  # first index is inclusive, second is exclusive
+x[2:4]
+
+# omitted indices default to beginning or end of list
+x[:3]
+x[5:]
+x[:]
+
+# negative indices count back from end of list
+x[-1]
+x[-5]
+x[4:-3]
+x[-3:-1]
+x[-3:0]  # result is an empty list when first index is after second index
+x[-3:]
+x[:-4]
+
+# - addition and multiplication
+
+a = [10, 20, 30] + [40, 50, 60]  # concatenates lists
+b = 3 * [10, 20, 30]             # repeats a list
+
+# - length, minimum, and maximum
+
+len(x)
+min(x)
+max(x)
+
+# - item assignment
+
+x = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+x[1] = 50
+x[20] = 50  # error; can't assign to a position that doesn't already exist
+
+# - assigning to slices
+
+x[0:3] = [100, 200, 300]
+x[0:3] = [100, 200, 300, 400, 500]  # can change number of elements
+
+# - deleting elements and slices
+
+del x[3]
+del x[3:5]
+
+# - list methods
+
+x = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+# adding elements
+x.append(110)                # appends value; changes x in place
+x.extend([1000, 2000, 3000]) # appends multiple values
+
+# reordering elements
+x.sort()        # sorts list in place
+y = sorted(x)   # returns a sorted copy of x, and leaves x unchanged
+
+# as mentioned above, lists can contain any data type
+y = [0, 0.0, True, [10,20,30]]
+len(y)
+type(y[0])
+type(y[3])
+
+# 4. Tuples
+
+# - lists are mutable; you can change their contents
+# - tuples are immutable
+
+x = (10, 20, 30)
+x = 10, 20, 30      # for tuples, the parentheses are optional
+type(x)
+
+# we can use many operations from lists on tuples too
+
+x[1]                # index
+x[1:3]              # slice
+x + x               # concatenate
+3*x                 # repeat
+len(x)              # length
+max(x)              # maximum
+
+# but we can't do anything that would change a tuple
+
+del x[0]            # error
+x[0] = 100          # error
+x.append(50)        # error
---
+++ 01b - lists/practice1b.py
@@ -0,0 +1,52 @@
+# practice1b.py  Practice exercises for lecture 1b
+
+# Add code below the following comments, to solve the problems.
+
+# 1. Create a list x that contains the numbers 100 to 110.
+
+# 2. Set a variable y equal to the following elements or sublists of x. When
+# possible, write your code so that it works for any length of x, e.g., it still
+# works if x has 20 elements rather than 11.
+
+# 2a. the first element of x
+
+# 2b. the first five elements of x
+
+# 2c. the last element of x
+
+# 2d. the last five elements of x
+
+# 2e. the first three elements of x and the last three elements of x
+
+# 2f. the first five elements of x, repeated ten times, i.e., the list
+#     [ 100, ..., 104 ], repeated ten times
+
+# 2g. the largest element of x, repeated five times
+
+# 2h. the smallest element of x, repeated a number of times equal to
+#     the largest element of x
+
+# 3. Change the list x as follows.
+
+# 3a. set the first element to zero
+
+# 3b. set the last three elements to zero
+
+# 3c. delete the fourth and fifth elements of x
+
+# 3d. delete the last element of x
+
+# 3e. append the value 120 to the end of x
+
+# 3f. append the first three elements of x to the end of x
+
+# 3g. sort the elements of x
+
+# 4. Create a tuple z that contains the following elements.
+
+# 4a. the number 50
+
+# 4b. the number 50, repeated three times
+
+# 4c. the numbers 1, 2, and 3, in that order, repeated five times
+
