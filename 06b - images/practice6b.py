b3696850f13eeb92ddc5b1053c688abfcbefb797
Richard Murray <rfm@yorku.ca>
2024-02-14 22:27:16 -0500

    posted practice problem

---
+++ 06b - images/practice6b.py
@@ -0,0 +1,15 @@
+# practice6b.py  Lecture 6b practice problem
+
+# This problem is a bit harder than the ones I've assigned so far, but
+# give it a try. We've covered everything you need to know to solve it,
+# but it will involve bringing together things you've learned about
+# psychopy and numpy.
+
+# Write a program that uses psychopy to show an endless movie of
+# random noise. The program will loop forever (i.e., while True),, and on
+# each frame, you will use numpy to create a 256 x 256 array of random
+# numbers between -1 and 1, and then draw it to the window just like the
+# lecture code drew the picture of Einstein. Do this in an infinite loop.
+# Allow the user to press any key to quit the loop and end the program.
+
+# Good luck!
