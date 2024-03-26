fe9cb3a33a815960af7567cacac623a2b9dc6718
Richard Murray <rfm@yorku.ca>
2024-03-26 09:00:07 -0400

    added pivot demo

--- 10b - data analysis/anova_demo.py
+++ 10b - data analysis/anova_demo.py
@@ -12,3 +12,14 @@ anova = AnovaRM(data=df, depvar='height', within=['colour','temperature'],
     subject='subject')
 result = anova.fit()
 print(result)
+
+# first step towards plotting data by category
+
+df2 = df.pivot(index='subject', columns=['colour', 'temperature'], values='height')
+print(df2)
+
+mu = df2.mean()
+print(mu)
+
+sigma = df2.std()
+print(sigma)
