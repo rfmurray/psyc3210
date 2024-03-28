d7ce17e1f742ab20d454ca9facf48792ac70ab30
Richard Murray <rfm@yorku.ca>
2024-03-28 08:29:16 -0400

    revised t test

--- 10b - data analysis/ttest_demo.py
+++ 10b - data analysis/ttest_demo.py
@@ -1,4 +1,4 @@
-# t_demo.py  Dependent samples t-test
+# ttest_demo.py  Dependent samples t-test
 
 import pandas as pd
 from scipy.stats import ttest_rel
@@ -6,6 +6,8 @@ from scipy.stats import ttest_rel
 # read data file into a data frame
 df = pd.read_csv('data.csv')
 print(df)
+
+# version 1
 
 # get measurement 1
 s1 = df.loc[ (df['colour']=='red') & (df['temperature']=='cold'), 'height']
@@ -17,4 +19,16 @@ print(s2)
 
 # run t-test
 result = ttest_rel(s1, s2)
+print(result)
+
+# version 2
+
+# pivot so that measurements of interest are in columns
+df2 = df.pivot(index='subject', columns=['colour', 'temperature'], values='height')
+print(df2)
+
+# re-run t-test
+x1 = df2[('red','cold')]
+x2 = df2[('red','hot')]
+result = ttest_rel(x1, x2)
 print(result)
