cee31c05cf3cad1178117d0f68809336f750f836
Richard Murray <rfm@yorku.ca>
2024-04-01 20:30:59 -0400

    added commands that allow imageio download on macOS

--- 10b - data analysis/configure.py
+++ 10b - data analysis/configure.py
@@ -1,7 +1,14 @@
 
+# some people find this step necessary on macOS in order for the operating
+# system to allow you to download the imageio extension below
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# install an extension to imageio that allows you to read .hdr image files
 import imageio
 imageio.plugins.freeimage.download()
 
+# install psychopy-minolta and statsmodels modules
 import sys, subprocess
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'psychopy-minolta'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'statsmodels'])
